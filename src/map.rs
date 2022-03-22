9bb50f8763f32aa8ad3487f3df74e4c698e3847b
yngtdd <yngtdd@gmail.com>
2022-03-22 17:53:49 -0400

    Add bounds checking for the map

--- src/map.rs
+++ src/map.rs
@@ -19,6 +19,7 @@ impl Map {
         }
     }
 
+    /// Render the map to the screen 
     pub fn render(&self, ctx: &mut BTerm) {
         for y in 0..SCREEN_HEIGHT {
             for x in 0..SCREEN_WIDTH {
@@ -33,6 +34,36 @@ impl Map {
                     }
                 }
             }
+        }
+    }
+
+    /// Check to see if a point is in bounds of the map
+    ///
+    /// We are in bounds if a point's (x, y) coordinates are greater than zero,
+    /// the x-coordinate is less than the `SCREEN_WIDTH` and the y-coordinate
+    /// is less than the `SCREEN_HEIGHT`
+    pub fn in_bounds(&self, point: Point) -> bool {
+        point.x >= 0 && point.x < SCREEN_WIDTH && point.y >= 0 && point.y < SCREEN_HEIGHT
+    }
+
+    /// Check if an entity can enter a tile
+    ///
+    /// An entity can enter a tile if it is within the map's bounds and the tile 
+    /// type is floor
+    pub fn can_enter_tile(&self, point: Point) -> bool {
+        self.in_bounds(point) && self.tiles[map_idx(point.x, point.y)] == TileType::Floor
+    }
+
+    /// Try to get the index of a particular tile on the map
+    ///
+    /// Given a particular `Point`, try to get the tile index. If the 
+    /// point is not within the map's bounds, return None, otherwise 
+    /// get the `map_index(point.x, point.y)`.
+    pub fn try_idx(&self, point: Point) -> Option<usize> {
+        if !self.in_bounds(point) {
+            None
+        } else {
+            Some(map_idx(point.x, point.y))
         }
     }
 }
