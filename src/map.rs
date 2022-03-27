4843408673f1e04c72ce134959ff693a39a9182b
yngtdd <yngtdd@gmail.com>
2022-03-27 19:02:46 -0400

    Add rendering system

--- src/map.rs
+++ src/map.rs
@@ -19,39 +19,6 @@ impl Map {
         }
     }
 
-    /// Render the map to the screen
-    ///
-    pub fn render(&self, ctx: &mut BTerm, camera: &Camera) {
-        ctx.set_active_console(0);
-        for y in camera.top_y..camera.bottom_y {
-            for x in camera.left_x..camera.right_x {
-                if self.in_bounds(Point::new(x, y)) {
-                    let idx = map_idx(x, y);
-                    match self.tiles[idx] {
-                        TileType::Floor => {
-                            ctx.set(
-                                x - camera.left_x,
-                                y - camera.top_y,
-                                WHITE,
-                                BLACK,
-                                to_cp437('.'),
-                            );
-                        }
-                        TileType::Wall => {
-                            ctx.set(
-                                x - camera.left_x,
-                                y - camera.top_y,
-                                WHITE,
-                                BLACK,
-                                to_cp437('#'),
-                            );
-                        }
-                    }
-                }
-            }
-        }
-    }
-
     /// Check to see if a point is in bounds of the map
     ///
     /// We are in bounds if a point's (x, y) coordinates are greater than zero,
