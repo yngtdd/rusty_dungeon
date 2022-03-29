09feadd0a195ae924f91adb96b967481a7a9af54
yngtdd <yngtdd@gmail.com>
2022-03-29 16:08:29 -0400

    chore: trim whitespace with cargo fmt

--- src/systems/random_move.rs
+++ src/systems/random_move.rs
@@ -6,20 +6,17 @@ use crate::prelude::*;
 pub fn random_move(ecs: &mut SubWorld, #[resource] map: &Map) {
     let mut movers = <(&mut Point, &MovingRandomly)>::query();
 
-    movers
-        .iter_mut(ecs)
-        .for_each(|(pos, _)| {
-            let mut rng = RandomNumberGenerator::new();
-            let destination = match rng.range::<u8>(0, 4) {
-                0 => Point::new(-1, 0),
-                1 => Point::new(1, 0),
-                2 => Point::new(0, -1),
-                _ => Point::new(0, 1),
-            } + *pos;
+    movers.iter_mut(ecs).for_each(|(pos, _)| {
+        let mut rng = RandomNumberGenerator::new();
+        let destination = match rng.range::<u8>(0, 4) {
+            0 => Point::new(-1, 0),
+            1 => Point::new(1, 0),
+            2 => Point::new(0, -1),
+            _ => Point::new(0, 1),
+        } + *pos;
 
-            if map.can_enter_tile(destination) {
-                *pos = destination;
-            }
+        if map.can_enter_tile(destination) {
+            *pos = destination;
         }
-    );
+    });
 }
