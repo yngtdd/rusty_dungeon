74eee47361329c7e0e9ab5dac4b6652833ea1eb0
yngtdd <yngtdd@gmail.com>
2022-03-29 13:46:02 -0400

    docs: add component docstrings

--- src/components.rs
+++ src/components.rs
@@ -1,16 +1,20 @@
 pub use crate::prelude::*;
 
+/// Rendering 2D glyphs
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Render {
     pub color: ColorPair,
     pub glyph: FontCharType,
 }
 
+/// Our hero
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Player;
 
+/// Enemies
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Enemy;
 
+/// Random movement of entities
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct MovingRandomly;
