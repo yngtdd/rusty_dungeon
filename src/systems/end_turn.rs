9cbc19a006d820b51ddbfc81d7aad918f148f445
yngtdd <yngtdd@gmail.com>
2022-03-29 18:38:28 -0400

    fix(state machine): Await input after monster turn
    Previously, this was going to the player state, no input was received
    and the monsters were running around like crazy.

--- src/systems/end_turn.rs
+++ src/systems/end_turn.rs
@@ -5,7 +5,7 @@ pub fn end_turn(#[resource] turn_state: &mut TurnState) {
     let new_state = match turn_state {
         TurnState::AwaitingInput => return,
         TurnState::PlayerTurn => TurnState::MonsterTurn,
-        TurnState::MonsterTurn => TurnState::PlayerTurn,
+        TurnState::MonsterTurn => TurnState::AwaitingInput,
     };
 
     *turn_state = new_state;
