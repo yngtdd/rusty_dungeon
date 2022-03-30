f1bb755e53877338487f674844b0e35e5151bfd4
yngtdd <yngtdd@gmail.com>
2022-03-30 00:20:00 -0400

    feat(components): Add health point component

--- src/components.rs
+++ src/components.rs
@@ -24,3 +24,9 @@ pub struct WantsToMove {
     pub entity: Entity,
     pub destination: Point,
 }
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Health {
+    pub current: i32,
+    pub max: i32,
+}
