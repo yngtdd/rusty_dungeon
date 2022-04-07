d98cc9f6d1fb8bf75eefbb13176c2bcc7347fd4e
yngtdd <yngtdd@gmail.com>
2022-04-06 23:57:18 -0400

    feat(render): Give the hero memory of the map
    We now render the map for tiles that the hero has seen but which
    are outside of the hero's field of view as a dark grey tile.

--- src/systems/map_render.rs
+++ src/systems/map_render.rs
@@ -15,14 +15,37 @@ pub fn map_render(ecs: &SubWorld, #[resource] map: &Map, #[resource] camera: &Ca
             let pt = Point::new(x, y);
             let offset = Point::new(camera.left_x, camera.top_y);
 
-            if map.in_bounds(pt) && player_fov.visible_tiles.contains(&pt) {
-                let idx = map_idx(x, y);
-                let glyph = match map.tiles[idx] {
-                    TileType::Floor => to_cp437('.'),
-                    TileType::Wall => to_cp437('#'),
+            let idx = map_idx(x, y);
+            if map.in_bounds(pt) && player_fov.visible_tiles.contains(&pt) | map.revealed_tiles[idx] {
+
+                let tint = if player_fov.visible_tiles.contains(&pt) {
+                    WHITE
+                } else {
+                    DARK_GREY
                 };
 
-                draw_batch.set(pt - offset, ColorPair::new(WHITE, BLACK), glyph);
+                match map.tiles[idx] {
+                    TileType::Floor => {
+                        draw_batch.set(
+                            pt - offset ,
+                            ColorPair::new(
+                                tint,
+                                BLACK
+                            ),
+                            to_cp437('.'),
+                        )
+                    }
+                    TileType::Wall => {
+                        draw_batch.set(
+                            pt - offset,
+                            ColorPair::new(
+                                tint,
+                                BLACK
+                            ),
+                            to_cp437('#'),
+                        )
+                    }
+                };
             }
         }
     }
