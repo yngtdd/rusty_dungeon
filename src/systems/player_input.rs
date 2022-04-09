a6585de011f2d195b0cb15640a9126aa1856a2b7
yngtdd <yngtdd@gmail.com>
2022-04-09 10:50:33 -0400

    refactor: Remove idle healing
    Since we have the ability to use potions now, we no longer need
    to provide a way for the hero to heal themself by hiding and doing
    nothing.

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -55,7 +55,6 @@ pub fn player_input(
             .unwrap();
 
         let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
-        let mut did_something = false;
 
         if delta.x != 0 || delta.y != 0 {
             let mut hit_something = false;
@@ -64,7 +63,6 @@ pub fn player_input(
                 .filter(|(_, pos)| **pos == destination)
                 .for_each(|(entity, _)| {
                     hit_something = true;
-                    did_something = true;
                     commands.push((
                         (),
                         WantsToAttack {
@@ -75,7 +73,6 @@ pub fn player_input(
                 });
 
             if !hit_something {
-                did_something = true;
                 commands.push((
                     (),
                     WantsToMove {
@@ -83,16 +80,6 @@ pub fn player_input(
                         destination,
                     },
                 ));
-            }
-        }
-
-        if !did_something {
-            if let Ok(mut health) = ecs
-                .entry_mut(player_entity)
-                .unwrap()
-                .get_component_mut::<Health>()
-            {
-                health.current = i32::min(health.max, health.current + 1);
             }
         }
 
