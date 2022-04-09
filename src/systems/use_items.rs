499df7c8448fa144acf48be611871ab84d3c32a5
yngtdd <yngtdd@gmail.com>
2022-04-09 10:51:42 -0400

    chore: Clean up with cargo fmt

--- src/systems/use_items.rs
+++ src/systems/use_items.rs
@@ -5,14 +5,11 @@ use crate::prelude::*;
 #[read_component(ProvidesHealing)]
 #[read_component(Health)]
 #[read_component(ProvidesDungeonMap)]
-pub fn use_items(
-    ecs: &mut SubWorld,
-    commands: &mut CommandBuffer,
-    #[resource] map: &mut Map
-) {
+pub fn use_items(ecs: &mut SubWorld, commands: &mut CommandBuffer, #[resource] map: &mut Map) {
     let mut healing_to_apply = Vec::<(Entity, i32)>::new();
 
-    <(Entity, &ActivateItem)>::query().iter(ecs)
+    <(Entity, &ActivateItem)>::query()
+        .iter(ecs)
         .for_each(|(entity, activate)| {
             let item = ecs.entry_ref(activate.item);
             if let Ok(item) = item {
@@ -33,12 +30,8 @@ pub fn use_items(
         // Gonna finally heal, Ryan.
         if let Ok(mut target) = ecs.entry_mut(heal.0) {
             if let Ok(health) = target.get_component_mut::<Health>() {
-                health.current = i32::min(
-                    health.max,
-                    health.current + heal.1
-                );
+                health.current = i32::min(health.max, health.current + heal.1);
             }
         }
     }
 }
-
